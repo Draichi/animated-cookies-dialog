26393b30732615588bdb0ec6f50a8e499ff9e90c
@Draichi <lucasdraichi@sabido.com>
2022-01-18 16:31:38 -0300

    feat: adds crumbs animation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ timeline.fromTo('.cookie-container', { scale: 0 }, { scale: 1, ease: 'elastic.ou
 timeline.fromTo('.cookie', { opacity: 0, x: -50, rotation: '-45deg' }, { opacity: 1, x: 0, rotation: '0deg' }, '<50%')
 timeline.fromTo('.text', { opacity: 0, x: 50 }, { opacity: 1, x: 0 }, '<')
 
+timeline.fromTo('.cookie', {y: 0, rotation: '0deg'}, {y: -20, rotation: '-20deg', yoyo: true, repeat: -1})
+timeline.fromTo('#crumbs', {y: 0}, {y: -20, yoyo: true, repeat: -1}, '<')
+
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
